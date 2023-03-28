3a329e214dd87367d263fa5c29d06f6f3d7e2066
JISHA SASIDHARAN <jishasasidharan778@gmail.com>
2023-03-28 18:04:26 +0530

    file added

---
+++ VariableExample.java
@@ -0,0 +1,23 @@
+class VariableExample
+{
+	public static void main(String args[])
+	{
+		int number=100;
+		char grade='A';
+		boolean status=true;
+		float marks=90.7f;
+		double salary=876542.99;
+   		byte age=22;
+		short s=10000;
+		long l=10L;
+	
+		System.out.println("Integer value:"+number);
+		System.out.println("Character value:"+grade);
+		System.out.println("Boolean value:"+status);
+		System.out.println("Float value:"+marks);
+		System.out.println("Double value:"+salary);
+		System.out.println("Byte value:"+age);
+		System.out.println("Short value:"+s);
+		System.out.println("Long value:"+l);
+	}
+}
