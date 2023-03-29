35a953e614c40e15cb7e7e715ac783d0a3280ff4
jishasasidharan778@gmail.com <jishasasidharan778@gmail.com>
2023-03-29 22:04:13 +0530

    add new file

---
+++ PriorityExample.java
@@ -0,0 +1,9 @@
+public class PriorityExample
+{
+	public static void main(String args[])
+	{
+		int num1=10,num2=20,num3=5,num4=6,num5=0;
+		num5=num1+num2*num3/num4;
+		System.out.println(num5);
+	}
+}
